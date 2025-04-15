47710a1f7c4d2cab00fcb0fcff4dea6cf3e7ddde
luciayin9944 <luxiayin94@gmail.com>
2025-04-14 18:55:43 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
+const userInfo = {
+  "username": "myname",
+  "password": "mypassword"
+};
+
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
+
+  const loginAttempt = (passwordAttempt) => {
+    if (isLocked) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      if (attemptCount >= 3) {
+        isLocked = true;
+        return "Attempt 3: Login failed";
+      }
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+
+  return loginAttempt;
+}
 
 
+// const tracker = createLoginTracker({
+//   username: "test",
+//   password: "123"
+// });
+// console.log(tracker("wrong1"));
+// console.log(tracker("123"));
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+
+
+//WRONG//
+// const userInfo = {
+//   username: "username",
+//   password: "password"
+// }
+
+
+// function createLoginTracker(username, passwordAttempt){
+//   let attemptCount = 0;
+
+//   while (attemptCount <=3) {
+//     if (passwordAttempt === userInfo.password){
+//       console.log("Login successful");
+//     } else {
+//       console.log("Login failed");
+//     };
+//     attemptCount++;
+//   }
+
+//   if (attemptCount>4) {
+//     console.log("Account locked due to too many failed login attempts" )
+//     return
+//   }
+// }
